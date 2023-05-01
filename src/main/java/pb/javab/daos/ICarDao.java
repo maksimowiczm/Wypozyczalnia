7e62c30652243e1c911b8d6facfc0be5064ee354
matmak13 <maksimowiczmateusz13@gmail.com>
2023-05-01 10:52:15 +0200

    daos interfaces

---
+++ src/main/java/pb/javab/daos/ICarDao.java
@@ -0,0 +1,6 @@
+package pb.javab.daos;
+
+import pb.javab.models.Car;
+
+public interface ICarDao extends IGenericDao<Car> {
+}
---
+++ src/main/java/pb/javab/daos/ICarRentalDao.java
@@ -0,0 +1,6 @@
+package pb.javab.daos;
+
+import pb.javab.models.CarRental;
+
+public interface ICarRentalDao extends IGenericDao<CarRental> {
+}
