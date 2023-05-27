85134c7fc46f0bab47c3fc96cd54f8ab209b3994
matmak13 <maksimowiczmateusz13@gmail.com>
2023-05-27 21:01:38 +0200

    db seed

--- src/main/java/Configuration.java
+++ src/main/java/Configuration.java
@@ -2,20 +2,22 @@ import jakarta.annotation.PostConstruct;
 import jakarta.ejb.Singleton;
 import jakarta.ejb.Startup;
 import jakarta.inject.Inject;
+import pb.javab.daos.ICarDao;
 import pb.javab.daos.IUserDao;
-import pb.javab.models.Role;
-import pb.javab.models.User;
+import pb.javab.models.*;
+
+import java.math.BigDecimal;
 
 @Singleton
 @Startup
 public class Configuration {
+    @Inject
     private IUserDao userDao;
+    @Inject
+    private ICarDao carDao;
 
     @PostConstruct
-    @Inject
-    public void init(IUserDao userDao) {
-        this.userDao = userDao;
-
+    public void init() {
         seedDatabase();
     }
 
@@ -25,5 +27,8 @@ public class Configuration {
         admin.setEmail("admin@a.aa");
         admin.setPassword("admin");
         userDao.save(admin);
+
+        var car = new Car("Matiz", "Daeweoo", 40, CarStatus.AVAILABLE, Transmission.MANUAL, new BigDecimal(100));
+        carDao.save(car);
     }
 }
