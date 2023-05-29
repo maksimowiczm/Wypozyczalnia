38d25028bf3fa297bc64eeeee30a93088ee0da60
matmak13 <maksimowiczmateusz13@gmail.com>
2023-05-29 14:48:15 +0200

    carrental getters setters

---
+++ src/main/java/pb/javab/services/CarRentalService.java
@@ -0,0 +1,32 @@
+package pb.javab.services;
+
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+import pb.javab.daos.CarRentalDao;
+import pb.javab.models.CarRental;
+import pb.javab.models.CarStatus;
+
+@Singleton
+public class CarRentalService implements ICarRentalService {
+    private final CarRentalDao carRentalDao;
+
+    @Inject
+    public CarRentalService(CarRentalDao carRentalDao) {
+        this.carRentalDao = carRentalDao;
+    }
+
+    @Override
+    public boolean rent(CarRental carRental) {
+        return false;
+    }
+
+    @Override
+    public boolean pay(CarRental carRental) {
+        return false;
+    }
+
+    @Override
+    public boolean cancel(CarStatus carRental) {
+        return false;
+    }
+}
