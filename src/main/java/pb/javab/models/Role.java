e70782be2bf2ba781dbdb8996be176ea13b85705
matmak13 <maksimowiczmateusz13@gmail.com>
2023-05-29 23:41:21 +0200

    AuthorizationFilterTests

--- src/main/java/pb/javab/models/Role.java
+++ src/main/java/pb/javab/models/Role.java
@@ -1,9 +1,6 @@
 package pb.javab.models;
 
-// niższa rola mniejsze uprawnienia! xd
-// Wyższa rola może robić to co niższa 🤡
 public enum Role {
     ADMIN,
     USER,
-    GUEST,
 }
--- src/test/java/pb/javab/filter/AuthorizationFilterTest.java
+++ src/test/java/pb/javab/filter/AuthorizationFilterTest.java
@@ -2,20 +2,16 @@ package pb.javab.filter;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.mockito.Mockito.*;
 import pb.javab.beans.UserBean;
 import pb.javab.models.Role;
 import pb.javab.models.User;
 
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 class AuthorizationFilterTest {
@@ -74,6 +70,30 @@ class AuthorizationFilterTest {
     @Test
     public void doFilter_guestRequestsForbiddenSite_callsResponseSendError() throws ServletException, IOException {
         when(userBean.getUser()).thenReturn(null);
+        when(request.getServletPath()).thenReturn("/views/admin");
+
+        new AuthorizationFilter(userBean).doFilter(request, response, chain);
+
+        verify(response, times(1)).sendError(404);
+    }
+
+    @Test
+    public void doFilter_userRequestsUserSite_callsChainDoFilter() throws ServletException, IOException {
+        var user = new User();
+        user.setRole(Role.USER);
+        when(userBean.getUser()).thenReturn(user);
+        when(request.getServletPath()).thenReturn("/views/user");
+
+        new AuthorizationFilter(userBean).doFilter(request, response, chain);
+
+        verify(chain, times(1)).doFilter(request, response);
+    }
+
+    @Test
+    public void doFilter_userRequestsForbiddenSite_callsResponseSendError() throws ServletException, IOException {
+        var user = new User();
+        user.setRole(Role.USER);
+        when(userBean.getUser()).thenReturn(user);
         when(request.getServletPath()).thenReturn("/views/admin");
 
         new AuthorizationFilter(userBean).doFilter(request, response, chain);
