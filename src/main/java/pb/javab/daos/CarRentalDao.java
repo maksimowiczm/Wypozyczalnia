12a73e044b01b2897befa1cd3c51188d1e44c54a
matmak13 <maksimowiczmateusz13@gmail.com>
2023-05-31 18:58:43 +0200

    get car rentals by user in dao

--- src/main/java/pb/javab/daos/CarRentalDao.java
+++ src/main/java/pb/javab/daos/CarRentalDao.java
@@ -1,12 +1,20 @@
 package pb.javab.daos;
 
 import jakarta.ejb.Stateless;
-import pb.javab.models.Car;
 import pb.javab.models.CarRental;
+import pb.javab.models.User;
+
+import java.util.List;
 
 @Stateless
 public class CarRentalDao extends GenericDao<CarRental> implements ICarRentalDao {
     public CarRentalDao() {
         super(CarRental.class);
+    }
+
+    @Override
+    public List<CarRental> getByUser(User user) {
+        // TODO
+        return null;
     }
 }
--- src/main/java/pb/javab/daos/ICarRentalDao.java
+++ src/main/java/pb/javab/daos/ICarRentalDao.java
@@ -1,6 +1,10 @@
 package pb.javab.daos;
 
 import pb.javab.models.CarRental;
+import pb.javab.models.User;
+
+import java.util.List;
 
 public interface ICarRentalDao extends IGenericDao<CarRental> {
+    List<CarRental> getByUser(User user);
 }
