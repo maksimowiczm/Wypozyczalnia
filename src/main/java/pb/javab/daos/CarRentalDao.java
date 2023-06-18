7b514c8840d1f97b82f933d1efe0e47d997f5fb3
matmak13 <maksimowiczmateusz13@gmail.com>
2023-06-18 18:17:49 +0200

    posprzatanie po niekompetencji

--- src/main/java/pb/javab/daos/CarRentalDao.java
+++ src/main/java/pb/javab/daos/CarRentalDao.java
@@ -3,7 +3,6 @@ package pb.javab.daos;
 import jakarta.ejb.Stateless;
 import pb.javab.models.CarRental;
 import pb.javab.models.CarRentalStatus;
-import pb.javab.models.User;
 
 import java.util.List;
 
@@ -11,12 +10,6 @@ import java.util.List;
 public class CarRentalDao extends GenericDao<CarRental> implements ICarRentalDao {
     public CarRentalDao() {
         super(CarRental.class);
-    }
-
-    @Override
-    public List<CarRental> getByUser(User user) {
-        // TODO
-        return null;
     }
 
     public List<CarRental> getByStatus(CarRentalStatus status) {
--- src/main/java/pb/javab/daos/ICarRentalDao.java
+++ src/main/java/pb/javab/daos/ICarRentalDao.java
@@ -2,12 +2,9 @@ package pb.javab.daos;
 
 import pb.javab.models.CarRental;
 import pb.javab.models.CarRentalStatus;
-import pb.javab.models.User;
 
 import java.util.List;
 
 public interface ICarRentalDao extends IGenericDao<CarRental> {
-    List<CarRental> getByUser(User user);
-
     List<CarRental> getByStatus(CarRentalStatus status);
 }
